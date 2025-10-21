8397ca3544aa32e088c0f2f445ceda147ea98e2b
dung27-vi <duvi57836@gmail.com>
2025-10-21 10:40:28 +0700

    BTVN006

---
+++ Untitled306.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float base, height, area;
+
+    printf("Nhap vao do dai canh day cua tam giac: ");
+    scanf("%f", &base);
+
+    printf("Nhap vao chieu cao cua tam giac: ");
+    scanf("%f", &height);
+
+    area = 0.5 * base * height;
+
+    printf("Dien tich tam giac la: %.2f\n", area);
+
+    return 0;
+}
